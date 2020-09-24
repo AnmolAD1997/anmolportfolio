887eb71623b826436acc9c8837d7a01cebe39485
Anmol Adhikari <adhikari.anmol97@gmail.com>
2020-09-23 22:01:09 -0500

    build

---
+++ build/precache-manifest.5196aeb21480c2d7c2a8a1502018dcfd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46f99c557e99ce178f9eb9247dc2b31d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "59ed9f4f436dbb94ce02",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "b6b8f9e1a7b05e846821",
+    "url": "/static/js/2.a2eb04cb.chunk.js"
+  },
+  {
+    "revision": "59ed9f4f436dbb94ce02",
+    "url": "/static/js/main.43564f2e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
